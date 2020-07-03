9545f4954d3a9673a94f0b16edf6b7a3d9e26fb3
mostafa soltani <soltani.mostafa@gmail.com>
2020-07-03 13:31:20 +0430

    springzuul

---
+++ src/test/java/com/mostafa/zuulapigetway/ZuulapigetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.zuulapigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulapigetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
